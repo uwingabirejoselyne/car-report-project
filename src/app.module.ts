dcdfce8aac4b71a4cb943e2bd9148f3586e91365
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-08 13:09:42 +0200

    connecting user  entity to the root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities : [],
+      entities : [User],
       synchronize : true
     }),
     UsersModule,
