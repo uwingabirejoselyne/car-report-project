e5b0f7627265702fde177d1d170209b4866da585
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-08 13:45:24 +0200

    making report entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,5 +1,9 @@
-import { Entity,Column,PrimaryGeneratedColumn } from "typeorm";
-
-export class Report{
-    
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column()
+  price: number;
 }
