e3537958ab11e1ba26dee7d87dc92790689b2823
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-08 12:03:16 +0200

    sqlite database connection

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities : [],
+      synchronize : true
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
