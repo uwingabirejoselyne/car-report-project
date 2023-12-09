686d7fd84340dec3eb7c945ad4974107f9694100
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-09 16:38:20 +0200

    connecting report module to its root

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -5,8 +5,8 @@ import { ReportsService } from './reports.service';
 import { Report } from './report.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Report])],
+  imports: [TypeOrmModule.forFeature([Report])],
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
