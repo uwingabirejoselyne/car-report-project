ababa7c0aa7b091f7974cb260192f7ee1ca80c0e
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-25 11:01:57 +0200

    create user update dto

--- src/users/dto/create.user.update.dto.ts
+++ src/users/dto/create.user.update.dto.ts
@@ -1,9 +1,16 @@
-import { IsEmail, IsNotEmpty, MinLength, IsString } from 'class-validator';
-
-export class CreateUserDto {
+import {
+  IsEmail,
+  IsString,
+  IsOptional,
+  IsNotEmpty,
+  MinLength,
+} from 'class-validator';
+export class createUserUpdate {
+  @IsOptional()
   @IsEmail({}, { message: 'Invalid email format' })
   readonly email: string;
 
+  @IsOptional()
   @IsString({ message: 'Password must be a string' })
   @IsNotEmpty({ message: 'Password should not be empty' })
   @MinLength(6, { message: 'Password should be at least 6 characters long' })
