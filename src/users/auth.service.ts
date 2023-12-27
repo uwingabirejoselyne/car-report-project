28c5f8f14ff75f5a3cd4b84da1a317946a1a455b
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-27 17:35:37 +0200

    checking if the email is in use

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+  async signup(email: string, password: string) {
+    const users = await this.userService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email is in use');
+    }
+  }
 }
