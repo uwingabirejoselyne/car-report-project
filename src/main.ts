1ce75e355be598b37772c9fbc770685c9af868bf
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-12-30 20:46:58 +0200

    installation of cookie

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+import cookieSession from 'cookie-session';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdfasdfd'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
